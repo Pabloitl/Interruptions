d580f6c5ea00ec8145c5ea80155e5ba1e57b2dea
Nacho1899 <74553384+Nacho1899@users.noreply.github.com>
2020-11-16 16:51:54 -0600

    Documentation

--- src/evaluator/EvaluatorApplier.java
+++ src/evaluator/EvaluatorApplier.java
@@ -1,28 +1,28 @@
 package evaluator;
 
-import java.io.File;
+import java.io.File; // Importamos la clase File
 
-import io.ExpressionReader;
-import io.ResultWriter;
+import io.ExpressionReader; // Importamos la clase ExpressionReader
+import io.ResultWriter; // Importamos la clase ResultWriter
 
 /**
  * EvaluatorApplier
  */
 public class EvaluatorApplier {
-    String in, out;
+    String in, out; // Declaramos las dos variables tipo string
 
-    public EvaluatorApplier(String in, String out) {
+    public EvaluatorApplier(String in, String out) { // Creamos el Constructor
         this.in = in;
         this.out = out;
     }
 
     public void apply() {
-        String[] expr = new ExpressionReader(in).getLines();
-        int[] results = new int[expr.length];
+        String[] expr = new ExpressionReader(in).getLines(); // Creamos un arrreglo de tipo String
+        int[] results = new int[expr.length]; // Creamos un arrreglo de tipo int
 
-        for (int i = 0; i < expr.length; ++i)
+        for (int i = 0; i < expr.length; ++i) // Con el for recorremos todo el arreglo para saber su tamaño
             results[i] = Evaluator.evaluate(expr[i]);
 
-        new ResultWriter(out).writeResults(results);
+        new ResultWriter(out).writeResults(results); // Creamos un nuevo objecto llamado ResultWriter
     }
 }
