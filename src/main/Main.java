617cb8601e38cdf2a7b68e264ced9d9d2d5d73f4
Nacho1899 <74553384+Nacho1899@users.noreply.github.com>
2020-11-16 16:34:29 -0600

    Documentation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,9 @@
 package main;
 
-import java.util.Scanner;
+import java.util.Scanner; // Importamos la clase Escaner
 
-import evaluator.Evaluator;
-import evaluator.EvaluatorApplier;
+import evaluator.Evaluator; // Importamos la clase Evauator
+import evaluator.EvaluatorApplier; // Importamos la clase EvaluatorApplier
 
 /**
  * Main
@@ -11,8 +11,8 @@ import evaluator.EvaluatorApplier;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // Se crea un objecto Scanner
 
-        new EvaluatorApplier(in.nextLine(), in.nextLine()).apply();
+        new EvaluatorApplier(in.nextLine(), in.nextLine()).apply(); // Se lee todo el objecto de la clase Applier
     }
 }
